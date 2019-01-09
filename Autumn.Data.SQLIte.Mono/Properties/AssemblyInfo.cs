ffe9698eed1407c222d126fae928757e53b2cdac
Dmitry Vysochin <dmitry.vysochin@gmail.com>
2019-01-09 15:59:01 +0200

    0.1.7

--- Autumn.Data.SQLIte.Mono/Properties/AssemblyInfo.cs
+++ Autumn.Data.SQLIte.Mono/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Autumn.Data.SQLIte.Mono")]
+[assembly: AssemblyTitle("Autumn.Net.Data.SQLIte.Mono")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Autumn.Data.SQLIte.Mono")]
+[assembly: AssemblyProduct("Autumn.Net.Data.SQLIte.Mono")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.7.0")]
+[assembly: AssemblyFileVersion("0.1.7.0")]
